b3a6651b51fc4043f048ca981a5ccd9403644b29
Abraham Jones <56662623+abejones26@users.noreply.github.com>
2020-02-22 08:39:59 -0600

    Final SQL Code

--- employees.sql
+++ employees.sql
@@ -1,24 +1,14 @@
+DROP TABLE departments;
+
 CREATE TABLE departments (
-	dept_noID PRIMARY KEY,
+	dept_noID SERIAL PRIMARY KEY,
 	dept_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE dept_emp (
-	emp_noID FOREIGN KEY,
-	dept_noID FOREIGN KEY,
-	from_date DATE,
-	to_date DATE
-);
-
-CREATE TABLE dept_manager (
-	dept_noID INTEGER FOREIGN KEY,
-	emp_noID INTEGER FOREIGN KEY,
-	from_date DATE,
-	to_date DATE
-);
+DROP TABLE employees;
 
 CREATE TABLE employees (
-	emp_noID PRIMARY KEY,
+	emp_noID SERIAL PRIMARY KEY,
 	birth_date DATE,
 	first_name VARCHAR(30) NOT NULL,
 	last_name VARCHAR(30) NOT NULL,
@@ -26,15 +16,43 @@ CREATE TABLE employees (
 	hire_date DATE
 );
 
-CREATE TABLE salaries (
-	emp_noID FOREIGN KEY,
-	salary DEC,
-	to_date DATE
+DROP TABLE dept_emp;
+
+CREATE TABLE dept_emp (
+	emp_noID INTEGER NOT NULL,
+	dept_noID INTEGER NOT NULL,
+	from_date DATE,
+	to_date DATE,
+	FOREIGN KEY (emp_noID) REFERENCES employees(emp_noID),
+	FOREIGN KEY (dept_noID) REFERENCES departments(dept_noID)
 );
 
+DROP TABLE dept_manager;
+
+CREATE TABLE dept_manager (
+	dept_noID INTEGER NOT NULL,
+	emp_noID INTEGER NOT NULL,
+	from_date DATE,
+	to_date DATE,
+	FOREIGN KEY (dept_noID) REFERENCES departments(dept_noID),
+	FOREIGN KEY (emp_noID) REFERENCES employees(emp_noID)
+);
+
+DROP TABLE salaries;
+
+CREATE TABLE salaries (
+	emp_noID INTEGER NOT NULL,
+	salary DEC,
+	to_date DATE,
+	FOREIGN KEY (emp_noID) REFERENCES employees(emp_noID)
+);
+
+DROP TABLE titles;
+
 CREATE TABLE titles (
-	emp_noID FOREIGN KEY,
+	emp_noID INTEGER NOT NULL,
 	title VARCHAR(30) NOT NULL,
 	to_date DATE,
-	PRIMARY KEY(title)
+	PRIMARY KEY (title),
+	FOREIGN KEY (emp_noID) REFERENCES employees(emp_noID)
 );
