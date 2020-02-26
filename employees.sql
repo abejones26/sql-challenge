34fd3d86189056f411145b1f378545cecf6bf390
Abraham Jones <56662623+abejones26@users.noreply.github.com>
2020-02-26 14:17:02 -0600

    Complete question seven

--- employees.sql
+++ employees.sql
@@ -90,6 +90,29 @@ INNER JOIN employees ON dept_emp."emp_noID" = employees."emp_noID";
 --5. List all employees whose first name is "Hercules" and last names begin with "B."
 SELECT * FROM employees
 WHERE first_name = 'Hercules'
-AND last_name = 'B*';
+AND last_name LIKE 'B%'
+
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT dept_emp."emp_noID", last_name, first_name, dept_name
+FROM dept_emp
+INNER JOIN departments ON dept_emp."dept_noID" = departments."dept_noID"
+INNER JOIN employees ON dept_emp."emp_noID" = employees."emp_noID"
+WHERE dept_name like 'Sales';
+
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT dept_emp."emp_noID", last_name, first_name, dept_name
+FROM dept_emp
+INNER JOIN departments ON dept_emp."dept_noID" = departments."dept_noID"
+INNER JOIN employees ON dept_emp."emp_noID" = employees."emp_noID"
+WHERE dept_name LIKE 'Sales'
+OR dept_name LIKE 'Development';
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+
+SELECT COUNT(last_name)
+FROM employees;
+
+
 
 
